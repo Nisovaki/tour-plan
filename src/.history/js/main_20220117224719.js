2f134f0778b81eacba779ab5b6aca9468e7b4f21
NisovaKi <oksy169516@gmail.com>
2022-01-17 23:32:03 +0300

    create: added swiper-slider in hotel section

---
+++ src/.history/js/main_20220117224719.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button--next',
+    prevEl: '.swiper-button--prev',
+  },
+
+  const swiper = new Swiper('.swiper', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
---
+++ src/.history/js/main_20220117224720.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button--next',
+    prevEl: '.swiper-button--prev',
+  },
+
+  const swiper = new Swiper('.swiper', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
+});
