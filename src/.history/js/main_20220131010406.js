4c2e83d12812f2a8ee96e4d77c5f6fc7a6ac2150
NisovaKi <oksy169516@gmail.com>
2022-01-31 01:58:58 +0300

    create: mobile menu

---
+++ src/.history/js/main_20220131010406.js
@@ -0,0 +1,38 @@
+var hotelSwiper = new Swiper('.hotel-swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-swiper__button--next',
+    prevEl: '.hotel-swiper__button--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+});
+
+var reviewsSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+});
+
+// parallax
+$('.parallax-window').parallax({ imageSrc: 'img/newsletter/newsletter-bg.jpeg' });
+
+var menuButton = document.querySelector(".menu-bottom");
